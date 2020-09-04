a1780510478dcb7e3753ba0557a283e909ccebda
Ong Beng Tiong <bengtiong@gmail.com>
2020-09-04 15:16:14 +0800

    ng update @angular/cli@8 @angular/core@8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ import { WelcomeComponent } from "./welcome/welcome.component";
 
 const appRoutes: Routes = [
   { path: 'app', component: WelcomeComponent },
-  { path: 'ships', loadChildren: './starships/starships.module#StarshipsModule' },
-  { path: 'ghostships', loadChildren: './ghostships/ghostships.module#GhostshipsModule' },
+  { path: 'ships', loadChildren: () => import('./starships/starships.module').then(m => m.StarshipsModule) },
+  { path: 'ghostships', loadChildren: () => import('./ghostships/ghostships.module').then(m => m.GhostshipsModule) },
   { path: '', redirectTo: '/app', pathMatch: 'full' },
 ];
 //  { path: 'ships', loadChildren: () => import('./starships/starships.module').then(m => m.StarshipsModule) },
